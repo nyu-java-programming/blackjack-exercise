e403df12e29fcbd63bb95e85bd0a48d98ae97fde
bloombar <amos@cs.nyu.edu>
2022-02-02 22:12:48 -0500

    checking for busts

--- src/edu/nyu/cs/Blackjack.java
+++ src/edu/nyu/cs/Blackjack.java
@@ -1,5 +1,7 @@
 package edu.nyu.cs;
 
+import java.util.Scanner;
+
 /**
  * A variation of the game of Blackjack.  
  * Complete this program according to the instructions in the README.md file as well as within the given comments below.
@@ -14,7 +16,67 @@ public class Blackjack {
   public static void main(String[] args) throws Exception {
 
     // complete this function according to the instructions
+    Scanner scn = new Scanner(System.in);
+    System.out.println("Welcome to Blackjack!");
+    String cards = getCard() + "," + getCard();
+    displayCards(cards);
 
+    boolean wantsMore = true;
+    boolean isBust = false;
+    while (wantsMore && !isBust) {
+      System.out.println("Would you like to hit or stand?");
+      String response = scn.nextLine();
+      if (response.equals("hit")) {
+        cards += ", " + getCard();
+        displayCards(cards);
+
+        // check for bust
+        int total = getTotal(cards);
+        if (total > 21) {
+          isBust = true;
+          System.out.println("You have bust!");
+          System.out.println("Dealer wins!");
+        }
+      }
+      else {
+        wantsMore = false;
+      }
+    }
+  }
+
+  public static int getCard() {
+    int card = (int) (Math.random() * 10) + 2;
+    return card;
+  }
+
+  public static void displayCards(String cards) {
+    String[] cardsArr = cards.split(",");
+    String msg = "Your cards are: ";
+    for (int i=0; i<cardsArr.length; i++) {
+      if (i == 0) {
+        msg += cardsArr[i];
+      }
+      else if (cardsArr.length == 2 && i == 1) {
+        msg += " and " + cardsArr[i];
+      }
+      else if (i == cardsArr.length - 1) {
+        // last card
+        msg += ", and " + cardsArr[i];
+      }
+      else {
+        msg += ", " + cardsArr[i];
+      }
+    }
+    System.out.println(msg);
+  }
+
+  public static int getTotal(String cards) {
+    String[] cardsArr = cards.split(",");
+    int total = 0;
+    for (String card : cardsArr) {
+      total += Integer.parseInt(card.strip());
+    }
+    return total;
   }
 
 }
--- test/edu/nyu/cs/BlackjackTest.java
+++ test/edu/nyu/cs/BlackjackTest.java
@@ -6,21 +6,30 @@ import org.junit.Test;
 import org.junit.Rule;
 import static org.junit.Assert.*;
 import org.junit.contrib.java.lang.system.SystemOutRule; // system rules lib - useful for capturing system output
-// import static org.mockito.Mockito.*; // for mocking, stubbing, and spying
+import org.junit.contrib.java.lang.system.TextFromStandardInputStream;
+import static org.junit.contrib.java.lang.system.TextFromStandardInputStream.*;
+// import static org.mockito.Mockito.*;
+
 
 public class BlackjackTest {
+
+    @Rule
+    public final TextFromStandardInputStream systemInMock = emptyStandardInputStream();
 
     @Rule
     public final SystemOutRule systemOutRule = new SystemOutRule();
 
     @Test
-    public void testMain() {
+    public void testWelcomeMessage() {
+        systemInMock.provideLines("stand");
         systemOutRule.enableLog(); // start capturing System.out
         String[] args = {};
         try {
             Blackjack.main(args);
-            String actual = systemOutRule.getLogWithNormalizedLineSeparator();
-            String expected = "Hello world!\n";
+            String output = systemOutRule.getLogWithNormalizedLineSeparator().strip();
+            String[] lines = output.split("\n");
+            String actual = lines[0].strip();
+            String expected = "Welcome to Blackjack!";
             assertEquals(expected, actual);
         }
         catch (Exception e) {
@@ -28,5 +37,28 @@ public class BlackjackTest {
         }
 
     }
+
+    @Test
+    public void testUserBust() {
+        systemInMock.provideLines("hit", "hit", "hit", "hit", "hit", "hit", "hit", "hit", "hit", "hit", "hit");
+        systemOutRule.enableLog(); // start capturing System.out
+        String[] args = {};
+        try {
+            Blackjack.main(args);
+            String output = systemOutRule.getLogWithNormalizedLineSeparator().strip();
+            String[] lines = output.split("\n");
+            String actualSecondToLastLine = lines[lines.length -2].strip();
+            String actualLastLine = lines[lines.length -1].strip();
+            String expectedLastLine = "Dealer wins!";
+            String expectedSecondToLastLine = "You have bust!";
+            assertEquals(expectedLastLine, actualLastLine);
+            assertEquals(expectedSecondToLastLine, actualSecondToLastLine);
+        }
+        catch (Exception e) {
+            assert false; // fail the test if any exception occurs
+        }
+
+    }
+
 
 }
